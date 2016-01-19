c15f01e56f3b399a363169c3df5f21b017c6103d
Adrian Wisaksana <adrian.wisaksana@students.makeschool.com>
2016-01-18 18:06:28 -0800

    Add Formspree AJAX

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,20 @@
 
 $(document).ready(function () {
-
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "//formspree.io/adrian@beingadrian.com",
+            method: "POST",
+            data: {
+                email: $('.email').val(),
+                message: $('.message').val()
+            },
+            dataType: "json"
+        }).error(function(x, status, error) {
+            alert("Error");
+        }).success(function(data, status) {
+            alert("Cheers mate.")
+            $('form').find('.email, .message').val('');
+        });
+    });
 });
